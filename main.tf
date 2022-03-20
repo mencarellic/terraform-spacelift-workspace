ae1192842a54c18f291c867ac20c9a702574aedb
Carlo Mencarelli <carlo.mencarelli@billgo.com>
2022-03-19 22:36:17 -0600

    More stacks

--- main.tf
+++ main.tf
@@ -4,3 +4,24 @@ resource "spacelift_stack" "aws-development" {
   name              = "aws-development"
   terraform_version = var.terraform-version
 }
+
+resource "spacelift_stack" "azure-development" {
+  repository        = "terraform-spacelift-workspace"
+  branch            = "main"
+  name              = "azure-development"
+  terraform_version = var.terraform-version
+}
+
+resource "spacelift_stack" "aws-production" {
+  repository        = "terraform-spacelift-workspace"
+  branch            = "main"
+  name              = "aws-production"
+  terraform_version = var.terraform-version
+}
+
+resource "spacelift_stack" "azure-production" {
+  repository        = "terraform-spacelift-workspace"
+  branch            = "main"
+  name              = "azure-production"
+  terraform_version = var.terraform-version
+}
